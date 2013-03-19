7e3905bfa3016586d51559f2ea12a84dba4f60c1
Vladimir Dronnikov <dronnikov@gmail.com>
2013-03-19 20:43:28 +0400

    Style

--- src/cowboy_social_google.erl
+++ src/cowboy_social_google.erl
@@ -26,76 +26,75 @@ terminate(_Reason, _Req, _State) ->
 handle(Req, Opts) ->
   % extract flow action name
   {Action, Req2} = cowboy_req:binding(action, Req),
-  % construct callback URI
-  {SelfUri, Req3} = cowboy_req:host_url(Req2),
-  [FullPath] = cowboy_req:get([path], Req3),
-  CallbackUrl = << SelfUri/binary, FullPath/binary >>,
   % perform flow action
-  {ok, Req4} = handle_request(Action, Opts, CallbackUrl, Req3),
-  {ok, Req4, undefined}.
+  {ok, Req3} = handle_request(Action, Req2, Opts),
+  {ok, Req3, undefined}.
 
 %%
 %% redirect to provider authorization page, expect it to redirect
 %% to our next handler
 %%
-handle_request(<<"login">>, Opts, U, Req)  ->
+handle_request(<<"login">>, Req, Opts)  ->
+  {SelfUri, Req2} = cowboy_req:host_url(Req),
+  CallbackUrl = << SelfUri/binary, (key(callback_uri, Opts))/binary >>,
   AuthUrl = << (authorize_url())/binary, $?,
       (cowboy_request:urlencode([
         {<<"client_id">>, key(client_id, Opts)},
-        {<<"redirect_uri">>, binary:replace(U, <<"/login">>, <<"/callback">>)},
+        {<<"redirect_uri">>, CallbackUrl},
         {<<"response_type">>, <<"code">>},
         {<<"scope">>, key(scope, Opts)}
       ]))/binary >>,
-  cowboy_req:reply(303, [{<<"location">>, AuthUrl}], <<>>, Req);
+  cowboy_req:reply(303, [{<<"location">>, AuthUrl}], <<>>, Req2);
 
 %%
 %% provider redirected back to us with authorization code
 %%
-handle_request(<<"callback">>, Opts, U, Req) ->
+handle_request(<<"callback">>, Req, Opts) ->
   case cowboy_req:qs_val(<<"code">>, Req) of
     {undefined, Req2} ->
       finish({error, nocode}, Req2, Opts);
     {Code, Req2} ->
-      get_access_token(Opts, U, Code, Req2)
+      get_access_token(Code, Req2, Opts)
   end;
 
 %%
 %% catchall
 %%
-handle_request(_, _, _, Req) ->
+handle_request(_, Req, _) ->
   {ok, Req2} = cowboy_req:reply(404, [], <<>>, Req),
   {ok, Req2, undefined}.
 
 %%
 %% exchange authorization code for auth token
 %%
-get_access_token(Opts, U, Code, Req) ->
+get_access_token(Code, Req, Opts) ->
+  {SelfUri, Req2} = cowboy_req:host_url(Req),
+  CallbackUrl = << SelfUri/binary, (key(callback_uri, Opts))/binary >>,
   case cowboy_request:post_for_json(token_url(), [
       {<<"code">>, Code},
       {<<"client_id">>, key(client_id, Opts)},
       {<<"client_secret">>, key(client_secret, Opts)},
-      {<<"redirect_uri">>, U},
+      {<<"redirect_uri">>, CallbackUrl},
       {<<"grant_type">>, <<"authorization_code">>}
     ])
   of
     {ok, Auth} ->
-      get_user_profile(Opts, Auth, Req);
+      get_user_profile(Auth, Req2, Opts);
     _ ->
-      finish({error, notoken}, Req, Opts)
+      finish({error, notoken}, Req2, Opts)
   end.
 
 %%
 %% use auth tocken to extract info from user profile
 %%
-get_user_profile(Opts, Auth, Req) ->
+get_user_profile(Auth, Req, Opts) ->
   AccessToken = key(<<"access_token">>, Auth),
   case cowboy_request:get_json(profile_url(), [
       {<<"access_token">>, AccessToken}
     ])
   of
     {ok, Profile} ->
-      finish({ok, normalize_profile(Auth, Profile)},
-          Req, Opts);
+      finish({ok, normalize_profile(Auth, Profile)}, Req, Opts);
     _ ->
       finish({error, noprofile}, Req, Opts)
   end.
@@ -104,7 +103,7 @@ get_user_profile(Opts, Auth, Req) ->
 %% finalize application flow by calling callback handler
 %%
 finish(Status, Req, Opts) ->
-  {M, F} = key(callback, Opts),
+  {M, F} = key(handler, Opts),
   M:F(Status, Req).
 
 %%
@@ -126,14 +125,13 @@ token_url() ->
 profile_url() ->
   <<"https://www.googleapis.com/oauth2/v1/userinfo">>.
 
-normalize_profile(_A, P) ->
+normalize_profile(_Auth, Raw) ->
   [
-    {id, << "google:", (key(<<"id">>, P))/binary >>},
+    {id, << "google:", (key(<<"id">>, Raw))/binary >>},
     {provider, <<"google">>},
-    {email, key(<<"email">>, P)},
-    {name, key(<<"name">>, P)},
-    {avatar, key(<<"picture">>, P)},
-    {gender, key(<<"gender">>, P)},
-    {locale, key(<<"locale">>, P)}
+    {email, key(<<"email">>, Raw)},
+    {name, key(<<"name">>, Raw)},
+    {avatar, key(<<"picture">>, Raw)},
+    {gender, key(<<"gender">>, Raw)},
+    {locale, key(<<"locale">>, Raw)}
   ].
-
